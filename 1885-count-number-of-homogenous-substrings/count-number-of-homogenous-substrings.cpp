4c26cf6922de1522041f8bc264c3fe6c3bf15e44
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-05-05 11:32:31 +0530

    Time: 11 ms (20.71%) | Memory: 15 MB (48.73%) - LeetSync

---
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int n=s.size();
+        int currentStreak=0;
+        int ans=0;
+        int MOD=1e9+7;
+        for(int i=0;i<s.size();i++){
+            if(i==0|| s[i]==s[i-1]){
+                currentStreak++;
+            }
+            else{
+                currentStreak=1;
+            }
+            ans=(ans+currentStreak)%MOD;
+        }
+        return ans;
+    }
+};
