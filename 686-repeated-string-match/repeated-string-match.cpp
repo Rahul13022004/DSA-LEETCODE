f1ba250c8e49d96d3e719b7dc92ebb14cb86845c
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-05-10 15:10:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (93.67%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        string repeated = a;
-        int count = 1;
+    // Build LPS array for KMP
+    void buildLPS(string &pat, vector<int> &lps) {
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+        while (i < pat.size()) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
 
-        // Repeat until repeated string length >= b
-        while (repeated.length() < b.length()) {
-            repeated += a;
-            count++;
+    int repeatedStringMatch(string a, string b) {
+        int m = a.length(), n = b.length();
+
+        // LPS for pattern b
+        vector<int> lps(n, 0);
+        buildLPS(b, lps);
+
+        // Try up to (n / m + 2) repetitions
+        int i = 0, j = 0, repeat = 1;
+        while (i < m * (n / m + 2)) {
+            if (a[i % m] == b[j]) {
+                i++;
+                j++;
+                if (j == n) {
+                    return (i + m - 1) / m; // Calculate how many times 'a' was used
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
         }
 
-        // Check if b is now a substring
-        if (repeated.find(b) != string::npos) return count;
-
-        // Check one more repetition (for overlaps like "abcd" + "abcd")
-        repeated += a;
-        if (repeated.find(b) != string::npos) return count + 1;
-
-        // Not found even after extra repetition
         return -1;
     }
 };
