518a7b49091053f815f77c062e4078288b626248
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-06-06 09:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.90%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> leftParen, stars;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') {
+                leftParen.push(i);
+            } else if (s[i] == '*') {
+                stars.push(i);
+            } else { // s[i] == ')'
+                if (!leftParen.empty()) {
+                    leftParen.pop();
+                } else if (!stars.empty()) {
+                    stars.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining '(' with '*' that come after them
+        while (!leftParen.empty() && !stars.empty()) {
+            if (leftParen.top() > stars.top()) return false;
+            leftParen.pop();
+            stars.pop();
+        }
+
+        return leftParen.empty();
+    }
+};
