ff902e71ea9133abec3b3264d0551df0498de7f5
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-06-17 10:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (60.46%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int AtMost(vector<int>& nums, int goal) {
-        int left = 0;
-        int count = 0;
-        int sum = 0;
-        for (int right = 0; right < nums.size(); right++) {
-            sum += nums[right];
-
-            while (left <= right && sum > goal) {
-                sum -= nums[left++];
+        int left=0;
+        int n=nums.size();
+        int sum=0;
+        int count=0;
+        for(int right=0;right<n;right++){
+            sum+=nums[right];
+            while(left<=right && sum>goal){
+                sum-=nums[left];
+                left++;
             }
-            count += right - left + 1;
+            count+=right-left+1;
         }
         return count;
     }
