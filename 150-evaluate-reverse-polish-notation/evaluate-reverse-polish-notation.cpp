3ede086576a9b683ea0fbc9ec7fd33f5cde213cb
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-06-23 13:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (35.86%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -3,25 +3,28 @@ public:// like postfix to infix
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
 
-        for (string c : tokens) {
-            if (c == "+") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first + second);
-            } else if (c == "-") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first - second);
-            } else if (c == "*") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first * second);
-            } else if (c == "/") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first / second);
-            } else {
-                st.push(stoi(c));
+        for(auto &it:tokens){
+            if(it=="+"){
+                int second=st.top();st.pop();
+                int first=st.top();st.pop();
+                st.push(first+second);
+            }
+            else if(it=="-"){
+                int second=st.top();st.pop();
+                int first=st.top();st.pop();
+                st.push(first-second);
+            }
+            else if(it=="*"){
+                int second=st.top();st.pop();
+                int first=st.top();st.pop();
+                st.push(first*second);
+            }
+            else if(it=="/"){
+                int second=st.top();st.pop();
+                int first=st.top();st.pop();
+                st.push(first/second);
+            }else{
+            st.push(stoi(it));
             }
         }
 
