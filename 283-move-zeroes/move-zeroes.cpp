150c5eb9b52091fe5c7b451d5b64da3fdc946004
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-06-29 00:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (50.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         int j=0;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                nums[j++]=nums[i];
-            }
+           if(nums[i]!=0){
+            nums[j++]=nums[i];
+           }
         }
         while(j<n){
             nums[j++]=0;
