4aaa310000ae7b5baadce904e0b7b40a96a364fc
Rahul Kumar <159282048+Rahul13022004@users.noreply.github.com>
2025-07-08 23:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (66.41%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,29 +10,31 @@
  */
 class Solution {
 public:
-    struct compare {
-        bool operator()(ListNode* a, ListNode* b) { return a->val > b->val; }
+    struct compare{
+        bool operator()(ListNode* a,ListNode* b){
+            return a->val>b->val;
+        }
     };
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
-
-        for (auto& it : lists) {
-            if (it)
-                pq.push(it);
+        priority_queue<ListNode*,vector<ListNode*>,compare>pq;
+        for(auto&it:lists){
+            if(it){
+            pq.push(it);
+            }
         }
-
         ListNode dummy(0);
-        ListNode* tail = &dummy;
-        while (!pq.empty()) {
-            ListNode* smallest = pq.top();
+        ListNode* tail=&dummy;
+        while(!pq.empty()){
+            ListNode* smallest=pq.top();
             pq.pop();
-            tail->next = smallest;
-            tail = tail->next;
-            if (smallest->next) {
+            tail->next=smallest;
+            tail=smallest;
+            if(smallest->next){
                 pq.push(smallest->next);
             }
         }
-        return dummy.next;
+
+      return dummy.next;
     }
 };
